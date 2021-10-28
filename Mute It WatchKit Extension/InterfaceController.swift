7dec5138bca02355e910295e70cb1faf04ea294a
Aleksandr Morozov <dr.morozov@hotmail.com>
2021-10-28 16:22:12 +0300

    Initial Commit

---
+++ Mute It WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Mute It WatchKit Extension
+//
+//  Created by Aleksandr Morozov on 28.10.2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
